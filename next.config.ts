1bbc31f28cc6c1a29629c68b82c3b0be80ab5dda
Tewodros Abebe <teddyab729@gmail.com>
2024-11-13 12:40:34 +0300

    Update the loading and Add Search functionality

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // config options here
 
+  // experimental: {
+  //   ppr: "incremental",
+  // },
+
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
